0d0536a427349f5c8e5a7e496f72b789c957b593
mahabubsheikhriyad <mahabubsheikh8765@gmail.com>
2024-06-27 17:47:47 +0600

    test3

--- TravelTogether/urls.py
+++ TravelTogether/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('accounts/', include('allauth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
